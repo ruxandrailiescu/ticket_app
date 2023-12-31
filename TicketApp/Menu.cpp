6eabacb817d36ee9327bdf7f385a03608392a272
Ruxandra Iliescu <iliescuruxandra23@stud.ase.ro>
2023-12-31 15:31:43 +0200

    Menu and main.cpp

--- TicketApp/Menu.cpp
+++ TicketApp/Menu.cpp
@@ -22,44 +22,93 @@ void Menu::display() {
 	cout << "|--------------------|\n";
 }
 
-void Menu::addEvent() {
-	string _id;
-	string _name;
-	string _location;
-	string _address;
-	int _maxNoSeats;
-
-	// have to do try-catch
-	// do-while
-	cout << endl << "Enter event id: ";
-	getline(cin, _id);
-	cout << endl << "Enter event name: ";
-	getline(cin, _name);
-	cout << endl << "Enter location: ";
-	getline(cin, _location);
-	cout << endl << "Enter address: ";
-	getline(cin, _address);
-	cout << "Enter max no of seats: ";
-	cin >> _maxNoSeats;
-
-	EventLocations l(_location, _address, _maxNoSeats);
-	Event e(_id, (char*)_name.c_str(), l);
-}
 void Menu::addEventLocation() {
-	string _location;
-	string _address;
+	cout << endl << "Location: ";
+	char buffer[256];
+	cin.ignore();
+	cin.getline(buffer, 256);
+	string _location = buffer;
+
+	cout << endl << "Address: ";
+	char buffer1[256];
+	cin.getline(buffer1, 256);
+	string _address = buffer1;
+	
 	int _maxNoSeats;
-	
-	// have to do try-catch
-	// do-while
-	cout << endl << "Enter location: ";
-	getline(cin, _location);
-	cout << endl << "Enter address: ";
-	getline(cin, _address);
-	cout << "Enter max no of seats: ";
+	cout << endl << "Maximum number of seats: ";
 	cin >> _maxNoSeats;
-	
-	EventLocations l(_location, _address, _maxNoSeats);
+
+	EventLocations* newLocation = new EventLocations(_location, _address, _maxNoSeats);
+	EventLocations::addEventLocation(*newLocation);
+}
+
+void Menu::addEvent() {
+	cout << endl << "Event id: ";
+	char buffer[256];
+	cin.ignore();
+	cin.getline(buffer, 256);
+	string _id = buffer;
+
+	cout << endl << "Event name: ";
+	char buffer1[256];
+	cin.getline(buffer1, 256);
+	char* _name = UtilTickets::copyString(buffer1);
+
+	EventLocations loc;
+	cin >> loc;
+
+	Event* newEvent = new Event(_id, _name, loc);
+	Event::addEvent(*newEvent);
+}
+
+void Menu::generateTicket() {
+	Event e;
+	cin >> e;
+
+	cout << endl << "Seat number: ";
+	char buffer[256];
+	cin.ignore();
+	cin.getline(buffer, 256);
+	char* _seatNumber = UtilTickets::copyString(buffer);
+
+	cout << endl << "Date: ";
+	char buffer1[256];
+	cin.getline(buffer1, 256);
+	char* _date = UtilTickets::copyString(buffer1);
+
+	cout << endl << "Time: ";
+	char buffer2[256];
+	cin.getline(buffer2, 256);
+	char* _time = UtilTickets::copyString(buffer2);
+
+	cout << endl << "Price: ";
+	double _price;
+	cin >> _price;
+
+	int type;
+	do {
+		cout << endl << "Enter ticket type (0 - Normal, 1 - Vip, 2 - Student): ";
+		cin >> type;
+	} while ((type != 0) && (type != 1) && (type != 2));
+	Ticketing* newTicket;
+
+	switch (type) {
+	case 0:
+		newTicket = new Normal(e, _seatNumber, _time, _date, _price);
+		Ticketing::addTicket(*newTicket);
+	case 1:
+		cout << endl << "Enter bonus points: ";
+		int _bonus;
+		cin >> _bonus;
+		newTicket = new Vip(e, _seatNumber, _time, _date, _price, _bonus);
+		Ticketing::addTicket(*newTicket);
+	case 2:
+		cout << endl << "Enter discount: ";
+		int _discount;
+		cin >> _discount;
+		newTicket = new Student(e, _seatNumber, _time, _date, _price, _discount);
+		Ticketing::addTicket(*newTicket);
+	}
 }
 
 void Menu::start() {
@@ -85,9 +134,9 @@ void Menu::start() {
 		case 3:
 			this->generateTicket();
 			break;
-		case 4:
-			this->validateTicket();
-			break;
+		//case 4:
+		//	this->validateTicket();
+		//	break;
 		}
 	} while (choice != 5);
 }
